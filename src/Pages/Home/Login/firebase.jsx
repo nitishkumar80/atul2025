27d19e713eb5d9d627257893214efd942e63f298
Atul2025-ars <itsupport@arskreedashala.com>
2024-11-23 23:05:22 +0530

    Update firebase.jsx

--- src/Pages/Home/Login/firebase.jsx
+++ src/Pages/Home/Login/firebase.jsx
@@ -8,7 +8,7 @@ import  "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA35yw088p0ls7u5nyxMbDTXK65gD0suy8",
+  apiKey: "",
   authDomain: "e-com-9579a.firebaseapp.com",
   projectId: "e-com-9579a",
   storageBucket: "e-com-9579a.appspot.com",
